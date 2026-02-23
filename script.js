78bf502a2b5ddd8aca02eef99666996142aaed05
Shadayday <Dayle_jl_mccrae@hotmail.co.uk>
2026-02-23 03:13:23 +0000

    Build basic todo app and fix input bug

---
+++ script.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addTaskBtn");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", function() {
+
+// 1. Get the value from the input    
+const taskText = input.value;
+
+console.log(taskText);
+
+// 2. If input is empty, do nothing
+if (taskText === "") {
+    return;
+
+}
+
+// 3. Create a new <li> element
+const li = document.createElement("li");
+
+// 4. Set the text of <li>
+li.textContent = taskText;
+
+// 5. Append <li> to the list
+list.appendChild(li);
+
+// 6. Clear the input
+input.value = "";
+
+});
+
+
