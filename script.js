b55eb660d84e270db74219f2f9d9eaf494ae7174
Shadayday <Dayle_jl_mccrae@hotmail.co.uk>
2026-02-23 12:43:33 +0000

    Persist tasks with localStorage

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ const input = document.getElementById("taskInput");
 const button = document.getElementById("addTaskBtn");
 const list = document.getElementById("taskList");
 
+let tasks = [];
+
+// Load saved tasks
+const savedTasks = localStorage.getItem("tasks");
+
+if (savedTasks) {
+    tasks = JSON.parse(savedTasks);
+    
+}
+
+function renderTasks() {
+    list.innerHTML = "";
+
+    tasks.forEach(function (task) {
+        const li = document.createElement("li");
+        li.textContent = task;
+        list.appendChild(li);
+    });
+}
+
+renderTasks();
+
 button.addEventListener("click", function() {
 
 // 1. Get the value from the input    
-const taskText = input.value;
+const taskText = input.value.trim();
 
 console.log(taskText);
 
@@ -15,14 +37,20 @@ if (taskText === "") {
 
 }
 
+tasks.push(taskText);
+
+localStorage.setItem("tasks", JSON.stringify(tasks));
+
+renderTasks();
+
 // 3. Create a new <li> element
-const li = document.createElement("li");
+//const li = document.createElement("li");
 
 // 4. Set the text of <li>
-li.textContent = taskText;
+//li.textContent = taskText;
 
 // 5. Append <li> to the list
-list.appendChild(li);
+//list.appendChild(li);
 
 // 6. Clear the input
 input.value = "";
@@ -35,4 +63,5 @@ input.addEventListener("keydown", function(event){
     }
 });
 
+document.body.style.backgroundColor = "lightblue";
 
